907b39a88981b0a335f2242025f2a68b934282fd
Cade Chaney <cadechaney@live.com>
2023-03-04 17:26:27 -0600

    refactor: Add changes to winning combos function in game.js

--- game.js
+++ game.js
@@ -1,13 +1,16 @@
 
+
+
 class Game {
     constructor(player1, player2) {
-        this.player1 = new Player('p1', './assets/scorpion.png')
-        this.player2 = new Player('p2', './assets/subzero.webp')
+        console.log(player1, player2)
+        this.player1 = player1
+        this.player2 = player2
         this.turn = player1
         this.winningTiles = [
-            [1,2,3], [4,5,6] [7,8,9], 
-            [1,4,7], [2,5,8] [3,6,9], 
-            [1,5,9], [3,5,7]
+            ['1', '2', '3'], ['4','5','6'], ['7','8','9'], 
+            ['1','4','7'], ['2','5','8'], ['3','6','9'], 
+            ['1','5','9'], ['3','5','7']
         ]
         this.winner = null
         this.moves = 0
@@ -24,16 +27,15 @@ class Game {
     winningCombos() {
         for (var i = 0; i < this.winningTiles.length; i++) {
             var combos = this.winningTiles[i]
-            var tile1 = document.getElementById(parseInt(combos[0])).innerHTML
-            var tile2 = document.getElementById(parseInt(combos[1])).innerHTML
-            var tile3 = document.getElementById(parseInt(combos[2])).innerHTML
-            if (this.player1.token === tile1 && this.player1.token === tile2 && this.player1.token === tile3) {
+            if (this.player1.selectedTiles.includes(combos[0]) && this.player1.selectedTiles.includes(combos[1]) && this.player1.selectedTiles.includes(combos[2])) {
                 this.winner = player1
-                player1.wins ++
+                player1.wins += 1
+                console.log('good job p1')
                 return 'Good Job p1'
-              } else if (this.player2.token === tile1 && this.player2.token === tile2 && this.player2.token === tile3) {
+              } else if (this.player2.selectedTiles.includes(combos[0]) && this.player2.selectedTiles.includes(combos[1]) && this.player2.selectedTiles.includes(combos[2]) ) {
                 this.winner = player2
-                player2.wins ++
+                player2.wins += 1
+                console.log('go player2')
                 return 'Good Job p2'
               } else {
                 this.gameDraw()
@@ -50,7 +52,8 @@ class Game {
         player2.wins = 0
         this.moves = 0
     }
-
-
+// max number of moves is 9 total. We need to log every move.
+// Everytime a click occurs it needs to run through the winning combos function.
 }
 //put winning combinations in an array
+
--- main.js
+++ main.js
@@ -1,45 +1,58 @@
 
+// './assets/scorpion.png'
+
 var player1 = new Player(1, './assets/scorpion.png');
 var player2 = new Player(2, './assets/subzero.webp');
+var game = new Game(player1, player2)
 
+
+var gamegrid = document.querySelector('.game-board')
 var buttons = document.querySelectorAll('.tile')
 var turnDisplay = document.querySelector('.heading')
+console.log(buttons)
 
-
-// var buttons.addEventListener ('click', addToken)
-
+// buttons.addEventListener('click', addToken)
+// gamegrid.addEventListener('click', addToken)
+// window.addEventListener('load', addEventListeners)
+//Global Variables
 var turn = player1;
-console.log(player1.tiles)
 
-
-// function addToken(event) {
-//     console.log('working')
-//     event.target.textContent = displayPlace()
-//     switchPlayerTurn()
+// function addEventListeners(event) {
+//     for (var i = 0; i < buttons.length; i++) {
+//         buttons[i].addEventListener('click', addToken(event))
+//     }
+    
 // }
 
-
-
-
-
+// function addToken() {
+//     console.log('it works')
+// }
+// function addToken(event) {
     for (var i = 0; i < buttons.length; i++) {
-      buttons[i].addEventListener('click', function(event){
-        console.log(this.turn)
-        event.target.innerHTML =  `<img class='token' src=${turn.token} />`
+      buttons[i].addEventListener('click', function(event) {
+        console.log(event.target)
+        event.target.innerHTML =  `<img class='token${event.target.id} piece' id=${turn.token} src=${turn.token} />`
+        game.turn.addTile(event.target.id)
         switchPlayerTurn()
-        // displayPlace()
+        game.winningCombos()
     })
 }
-
+// console.log(event.target)
+// }
 
 function switchPlayerTurn() {
     if (turn === player1) {
         turn = player2
+        game.switchTurn()
         console.log(this.turn)
     } else {
         turn = player1
+        game.switchTurn()
         console.log(this.turn)
     }
 }
 
+// function gameWon() {
+//     if ()
+// }
 // research .contains() in order to not have tiles be over riden by other tiles
