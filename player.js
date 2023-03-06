f28d5290155cb8bad7fe72cbfedda35ce1d6b41b
Cade Chaney <cadechaney@live.com>
2023-03-06 13:06:59 -0600

    refactor:Add changes to javascript files for readability

--- player.js
+++ player.js
@@ -1,17 +1,17 @@
 
 class Player {
     constructor(id, token) {
-      this.id = id
-      this.token = token
-      this.wins = 0
-      this.selectedTiles = []
+      this.id = id;
+      this.token = token;
+      this.wins = 0;
+      this.selectedTiles = [];
     }
 
     increaseWins() {
-        this.wins ++
+        this.wins ++;
     }
 
     addTile(tile) {
-        this.selectedTiles.push(tile)
+        this.selectedTiles.push(tile);
     }
 }
